71d194dbe78d534be264626ebf4604cb5fcb1f74
CoderguySuryansh <98791574+CoderguySuryansh@users.noreply.github.com>
2023-08-01 21:47:47 +0530

    Add files via upload

---
+++ Tic tae toe .cpp
@@ -0,0 +1,96 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// Function to print the Tic-Tac-Toe board
+void printBoard(const vector<vector<char>>& board) {
+    cout << "-------------" << endl;
+    for (int i = 0; i < 3; ++i) {
+        cout << "| ";
+        for (int j = 0; j < 3; ++j) {
+            cout << board[i][j] << " | ";
+        }
+        cout << endl;
+        cout << "-------------" << endl;
+    }
+}
+
+// Function to check if a player has won
+bool checkWin(const vector<vector<char>>& board, char player) {
+    for (int i = 0; i < 3; ++i) {
+        // Check rows and columns
+        if ((board[i][0] == player && board[i][1] == player && board[i][2] == player) ||
+            (board[0][i] == player && board[1][i] == player && board[2][i] == player)) {
+            return true;
+        }
+    }
+
+    // Check diagonals
+    if ((board[0][0] == player && board[1][1] == player && board[2][2] == player) ||
+        (board[0][2] == player && board[1][1] == player && board[2][0] == player)) {
+        return true;
+    }
+
+    return false;
+}
+
+// Function to check if the board is full (draw)
+bool checkDraw(const vector<vector<char>>& board) {
+    for (int i = 0; i < 3; ++i) {
+        for (int j = 0; j < 3; ++j) {
+            if (board[i][j] == ' ') {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+int main() {
+    vector<vector<char>> board(3, vector<char>(3, ' '));
+    char currentPlayer = 'X';
+
+    cout << "Tic-Tac-Toe Game" << endl;
+
+    while (true) {
+        printBoard(board);
+
+        // Get the current player's move
+        int row, col;
+        cout << "Player " << currentPlayer << ", enter your move (row[1-3] col[1-3]): ";
+        cin >> row >> col;
+
+        // Convert to 0-based index
+        --row;
+        --col;
+
+        // Check if the move is valid
+        if (row < 0 || row >= 3 || col < 0 || col >= 3 || board[row][col] != ' ') {
+            cout << "Invalid move. Try again." << endl;
+            continue;
+        }
+
+        // Make the move
+        board[row][col] = currentPlayer;
+
+        // Check if the current player wins
+        if (checkWin(board, currentPlayer)) {
+            printBoard(board);
+            cout << "Player " << currentPlayer << " wins!" << endl;
+            break;
+        }
+
+        // Check if it's a draw
+        if (checkDraw(board)) {
+            printBoard(board);
+            cout << "It's a draw!" << endl;
+            break;
+        }
+
+        // Switch to the other player
+        currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+    }
+
+    return 0;
+}
